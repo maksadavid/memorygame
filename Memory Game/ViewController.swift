7a851172a757f931b8f7c7d2fd95d4ea3aa39ac9
Dávid Maksa <maksadavid@gmail.com>
2020-07-30 11:47:08 +0200

    Initial Commit

---
+++ Memory Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memory Game
+//
+//  Created by David Maksa on 30.07.20.
+//  Copyright © 2020 David Maksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
